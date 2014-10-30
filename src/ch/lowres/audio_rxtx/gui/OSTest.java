4c6a923689f27046341995677f7bb4954b50ecab
Thomas Brand <tom@trellis.ch>
2014-10-30 17:13:41 +0100

    rudimentary arch test 32/64 bits

--- src/ch/lowres/audio_rxtx/gui/OSTest.java
+++ src/ch/lowres/audio_rxtx/gui/OSTest.java
@@ -16,30 +16,63 @@ package ch.lowres.audio_rxtx.gui;
 //========================================================================
 public class OSTest
 {
-	private String os;
 	private boolean isUnix;
 	private boolean isWindows;
 	private boolean isMac;
+	private boolean is32Bits;
+	private boolean is64Bits;
 
 //========================================================================
 	public OSTest()
 	{
 		determineOS();
+		determineArch();
 	}
 
 //========================================================================
 	public void determineOS()
 	{
-		os = System.getProperty("os.name").toLowerCase();
+		String os = System.getProperty("os.name").toLowerCase();
 		isUnix = os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0;
 		isWindows = os.indexOf("win") >= 0;
 		isMac = os.indexOf("mac") >= 0;
 	}
 
 //========================================================================
-	public String getName()
+	//https://community.oracle.com/thread/2086185?start=0&tstart=0
+	public void determineArch()
+	{
+		String bits = System.getProperty("sun.arch.data.model", "?");
+		if(bits.equals("64"))
+		{
+			is64Bits=true;
+			is32Bits=false;
+			return;
+		}
+		else if(bits.equals("?"))
+		{
+			// probably sun.arch.data.model isn't available
+			// maybe not a Sun JVM?
+			// try with the vm.name property
+			is64Bits=System.getProperty("java.vm.name").toLowerCase().indexOf("64") >= 0;
+			is32Bits=!is64Bits;
+			return;
+		}
+		// probably 32bit
+		is32Bits=true;
+		is32Bits=false;
+	}
+
+//========================================================================
+	public String getOSName()
 	{
 		return System.getProperty("os.name");
+	}
+
+//========================================================================
+	public String getVMName()
+	{
+		return System.getProperty("java.vm.name");
 	}
 
 //========================================================================
@@ -64,5 +97,17 @@ public class OSTest
 	boolean isWindows()
 	{
 		return isWindows;
+	}
+
+//========================================================================
+	boolean is32Bits()
+	{
+		return is32Bits;
+	}
+
+//========================================================================
+	boolean is64Bits()
+	{
+		return is64Bits;
 	}
 } //end class OSTest
