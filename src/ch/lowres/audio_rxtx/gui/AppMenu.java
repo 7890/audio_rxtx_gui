2698a599f6b6f7e25934cadcf794b54bac216059
Thomas Brand <tom@trellis.ch>
2014-11-04 19:02:19 +0100

    change to JMenu, JMenuItem, J* objects. add mnemonic to reach top-level menus. add help/report issue, help/check for updates

--- src/ch/lowres/audio_rxtx/gui/AppMenu.java
+++ src/ch/lowres/audio_rxtx/gui/AppMenu.java
@@ -23,37 +23,42 @@ import java.io.File;
 
 import com.magelang.tabsplitter.*;
 
+import javax.swing.*;
+
 //========================================================================
-public class AppMenu extends MenuBar
+public class AppMenu extends JMenuBar
 {
 	static Main g;
 
-	static Menu menu_main;
-	static Menu menu_settings;
-	static Menu menu_view;
-	static Menu menu_help;
+	static JMenu menu_main;
+	static JMenu menu_settings;
+	static JMenu menu_view;
+	static JMenu menu_help;
 
-	static MenuItem mi_start_transmissionSend;
-	static MenuItem mi_stop_transmissionSend;
+	static JMenuItem mi_start_transmissionSend;
+	static JMenuItem mi_stop_transmissionSend;
 
-	static MenuItem mi_start_transmissionReceive;
-	static MenuItem mi_stop_transmissionReceive;
+	static JMenuItem mi_start_transmissionReceive;
+	static JMenuItem mi_stop_transmissionReceive;
 
-	static MenuItem mi_quit;
+	static JMenuItem mi_quit;
 
-	static MenuItem mi_load_default_settings;
-	static MenuItem mi_save_default_settings;
-	static MenuItem mi_load_settings;
-	static MenuItem mi_save_settings_as;
-	static MenuItem mi_configure_dialog;
+	static JMenuItem mi_load_default_settings;
+	static JMenuItem mi_save_default_settings;
+	static JMenuItem mi_load_settings;
+	static JMenuItem mi_save_settings_as;
+	static JMenuItem mi_configure_dialog;
 
-	static MenuItem mi_view_send;
-	static MenuItem mi_view_receive;
-	static MenuItem mi_view_both;
+	static JMenuItem mi_view_send;
+	static JMenuItem mi_view_receive;
+	static JMenuItem mi_view_both;
 
-	static MenuItem mi_about;
-	static MenuItem mi_license;
-	static MenuItem mi_doc;
+	static JMenuItem mi_about;
+	static JMenuItem mi_license;
+	static JMenuItem mi_doc;
+
+	static JMenuItem mi_report_issue;
+	static JMenuItem mi_check_for_update;
 
 //========================================================================
 	public AppMenu()
@@ -67,10 +72,17 @@ public class AppMenu extends MenuBar
 	{
 		//http://stackoverflow.com/questions/17767950/java-menubar-cuts-into-my-applet
 
-		menu_main=new Menu("Main");
-		menu_settings=new Menu("Settings");
-		menu_view=new Menu("View");
-		menu_help=new Menu("Help");
+		menu_main=new JMenu("Main",true);
+		menu_main.setMnemonic(KeyEvent.VK_M);
+
+		menu_settings=new JMenu("Settings");
+		menu_settings.setMnemonic(KeyEvent.VK_S);
+
+		menu_view=new JMenu("View");
+		menu_view.setMnemonic(KeyEvent.VK_V);
+
+		menu_help=new JMenu("Help");
+		menu_help.setMnemonic(KeyEvent.VK_H);
 
 		add(menu_main);
 		add(menu_settings);
@@ -78,88 +90,101 @@ public class AppMenu extends MenuBar
 		add(menu_help);
 
 		//main items
-		mi_start_transmissionSend=new MenuItem("Start Transmission (Send)",
-			new MenuShortcut(KeyEvent.VK_A, false)
-		);
-		mi_stop_transmissionSend=new MenuItem("Stop Transmission (Send)",
-			new MenuShortcut(KeyEvent.VK_D, false)
-		);
+		mi_start_transmissionSend=new JMenuItem("Start Transmission (Send)");
+		mi_start_transmissionSend.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_MASK));
 
-		mi_start_transmissionReceive=new MenuItem("Start Transmission (Receive)",
-			new MenuShortcut(KeyEvent.VK_W, false)
-		);
-		mi_stop_transmissionReceive=new MenuItem("Stop Transmission (Receive)",
-			new MenuShortcut(KeyEvent.VK_R, false)
-		);
 
-		mi_quit=new MenuItem("Quit",
-			new MenuShortcut(KeyEvent.VK_Q, false)
-		);
+		mi_stop_transmissionSend=new JMenuItem("Stop Transmission (Send)");
+		mi_stop_transmissionSend.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_MASK));
+
+		mi_start_transmissionReceive=new JMenuItem("Start Transmission (Receive)");
+		mi_start_transmissionReceive.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_MASK));
+
+		mi_stop_transmissionReceive=new JMenuItem("Stop Transmission (Receive)");
+		mi_stop_transmissionReceive.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.CTRL_MASK));
+
+		mi_quit=new JMenuItem("Quit");
+		mi_quit.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_MASK));
 
 		menu_main.add(mi_start_transmissionSend);
 		menu_main.add(mi_stop_transmissionSend);
-		menu_main.add("-");
+		menu_main.add(new JSeparator());
 		menu_main.add(mi_start_transmissionReceive);
 		menu_main.add(mi_stop_transmissionReceive);
-
-
 		mi_stop_transmissionSend.setEnabled(false);
 		mi_stop_transmissionReceive.setEnabled(false);
-
-
-		menu_main.add(new MenuItem("-"));
+		menu_main.add(new JSeparator());
 		menu_main.add(mi_quit);
 
 		//settings items
-		mi_load_default_settings=new MenuItem("Load Default",
-			new MenuShortcut((KeyEvent.VK_COMMA), false)
-		);
-		mi_save_default_settings=new MenuItem("Save As Default",
-			new MenuShortcut(KeyEvent.VK_PERIOD, false)
-		);
-		mi_load_settings=new MenuItem("Load...",
-			new MenuShortcut(KeyEvent.VK_O, false)
-		);
-		mi_save_settings_as=new MenuItem("Save As...",
-			new MenuShortcut(KeyEvent.VK_S, false)
-		);
-		mi_configure_dialog=new MenuItem("Configure...",
-			new MenuShortcut(KeyEvent.VK_L, false)
-		);
+		mi_load_default_settings=new JMenuItem("Load Default");
+		mi_load_default_settings.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_COMMA, InputEvent.CTRL_MASK));
+
+		mi_save_default_settings=new JMenuItem("Save As Default");
+		mi_save_default_settings.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_PERIOD, InputEvent.CTRL_MASK));
+
+		mi_load_settings=new JMenuItem("Load...");
+		mi_load_settings.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
+
+		mi_save_settings_as=new JMenuItem("Save As...");
+		mi_save_settings_as.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+
+		mi_configure_dialog=new JMenuItem("Configure...");
+		mi_configure_dialog.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_MASK));
 
 		menu_settings.add(mi_load_default_settings);
 		menu_settings.add(mi_save_default_settings);
-		menu_settings.add(new MenuItem("-"));
+		menu_settings.add(new JSeparator());
 		menu_settings.add(mi_load_settings);
 		menu_settings.add(mi_save_settings_as);
-		menu_settings.add(new MenuItem("-"));
+		menu_settings.add(new JSeparator());
 		menu_settings.add(mi_configure_dialog);
 
 		//view items
-		mi_view_send=new MenuItem("Show Send",
-			new MenuShortcut(KeyEvent.VK_G, false)
-		);
-		mi_view_receive=new MenuItem("Show Receive",
-			new MenuShortcut(KeyEvent.VK_H, false)
-		);
-		mi_view_both=new MenuItem("Show Both",
-			new MenuShortcut(KeyEvent.VK_J, false)
-		);
+		mi_view_send=new JMenuItem("Show Send");
+		mi_view_send.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_G, InputEvent.CTRL_MASK));
+
+		mi_view_receive=new JMenuItem("Show Receive");
+		mi_view_receive.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_H, InputEvent.CTRL_MASK));
+
+		mi_view_both=new JMenuItem("Show Both");
+		mi_view_both.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_J, InputEvent.CTRL_MASK));
 
 		menu_view.add(mi_view_send);
 		menu_view.add(mi_view_receive);
 		menu_view.add(mi_view_both);
 
 		//help items
-		mi_about=new MenuItem("About...");
-		mi_license=new MenuItem("Liecense...");
-		mi_doc=new MenuItem("Manual (PDF)...",
-			new MenuShortcut(KeyEvent.VK_1, false)
-		);
+		mi_about=new JMenuItem("About...");
+		mi_license=new JMenuItem("Liecense...");
+		mi_doc=new JMenuItem("Manual (PDF)...");
+		mi_doc.setAccelerator(
+			KeyStroke.getKeyStroke(KeyEvent.VK_1, InputEvent.CTRL_MASK));
+
+		mi_report_issue=new JMenuItem("Report Issuse...");
+		mi_check_for_update=new JMenuItem("Check For Updates...");
 
 		menu_help.add(mi_about);
 		menu_help.add(mi_license);
 		menu_help.add(mi_doc);
+		menu_help.add(new JSeparator());
+		menu_help.add(mi_report_issue);
+//dummy
+		menu_help.add(mi_check_for_update);
+
 	}//end createMenu
 
 //========================================================================
@@ -415,18 +440,6 @@ public class AppMenu extends MenuBar
 		});
 
 		//help
-		mi_doc.addActionListener(new ActionListener()
-		{
-			@Override public void actionPerformed(ActionEvent e)
-			{
-				//ev simply open a textfile instead of pdf
-				String docPdf=g.tmpDir+File.separator+"resources"
-					+File.separator+"doc"+File.separator+"jack_audio_send.pdf";
-
-				g.p("opening file "+docPdf);
-				IOTools.openFile(new File(docPdf));
-			}
-		});
 
 		mi_about.addActionListener(new ActionListener()
 		{
@@ -447,6 +460,37 @@ public class AppMenu extends MenuBar
 				IOTools.openFile(new File(docTxt));
 			}
 		});
+
+		mi_doc.addActionListener(new ActionListener()
+		{
+			@Override public void actionPerformed(ActionEvent e)
+			{
+				//ev simply open a textfile instead of pdf
+				String docPdf=g.tmpDir+File.separator+"resources"
+					+File.separator+"doc"+File.separator+"jack_audio_send.pdf";
+
+				g.p("opening file "+docPdf);
+				IOTools.openFile(new File(docPdf));
+			}
+		});
+
+		mi_report_issue.addActionListener(new ActionListener()
+		{
+			@Override public void actionPerformed(ActionEvent e)
+			{
+				IOTools.openInBrowser(g.reportIssueUrl);
+			}
+		});
+
+		mi_check_for_update.addActionListener(new ActionListener()
+		{
+			@Override public void actionPerformed(ActionEvent e)
+			{
+				/////////test thread
+				IOTools.checkForNewerVersion(g.newestVersionFileUrl);
+			}
+		});
+
 	}//end addActionListeners
 
 //========================================================================
