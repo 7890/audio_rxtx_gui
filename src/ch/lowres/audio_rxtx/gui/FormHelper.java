e5c43c6fa4b8e64eb7ed227e4e93aa0a08beeb51
Thomas Brand <tom@trellis.ch>
2014-11-18 19:56:16 +0100

    initial add

---
+++ src/ch/lowres/audio_rxtx/gui/FormHelper.java
@@ -0,0 +1,31 @@
+/* part of audio_rxtx GUI
+ * https://github.com/7890/audio_rxtx_gui
+ *
+ * Copyright (C) 2014 Thomas Brand <tom@trellis.ch>
+ *
+ * This program is free software; feel free to redistribute it and/or 
+ * modify it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. bla.
+*/
+
+package ch.lowres.audio_rxtx.gui;
+
+import java.awt.*;
+
+class FormHelper
+{
+	static void validate(Container container)
+	{
+		Component c[]=container.getComponents();
+		for(int i=0;i<c.length;i++)
+		{
+		        if(c[i] instanceof TextFieldWithLimit)
+		        {
+				((TextFieldWithLimit)c[i]).validate_();
+		        }
+		}
+	}//end validateNumericTextFields
+}//end class FormHelper
