e1559b1dff0b5ef59a730a5156d348fd67a40f2d
Thomas Brand <tom@trellis.ch>
2014-12-22 23:15:57 +0100

    initial add, for status with delayed reset to default text

---
+++ src/ch/lowres/audio_rxtx/gui/widgets/StatusLabel.java
@@ -0,0 +1,75 @@
+/* part of audio_rxtx GUI
+ * https://github.com/7890/audio_rxtx_gui
+ *
+ * Copyright (C) 2014 Thomas Brand <tom@trellis.ch>
+ *
+ * This program is free software; feel free to redistribute it and/or 
+ * modify it.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. bla.
+*/
+
+package ch.lowres.audio_rxtx.gui.widgets;
+import ch.lowres.audio_rxtx.gui.*;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+
+/**
+* Extended ALabel for Status.
+*/
+//========================================================================
+public class StatusLabel extends ALabel
+{
+	Timer timer;
+
+//========================================================================
+	public StatusLabel()
+	{
+		super("");
+		init();
+	}
+
+//========================================================================
+	public StatusLabel(String label)
+	{
+		super(label);
+		init();
+	}
+
+//========================================================================
+	public StatusLabel(String label, int align)
+	{
+		super(label,align);
+		init();
+	}
+
+//========================================================================
+	void init()
+	{
+		setOpaque(false);
+
+		ActionListener resetStatus = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent evt)
+			{
+				StatusLabel.super.setText("Ready");
+			}
+		};
+		timer=new Timer(0, resetStatus);
+		timer.setRepeats(false);
+	}
+
+//========================================================================
+	public void setStatus(String message,int milliseconds)
+	{
+		super.setText(""+message);
+		timer.setInitialDelay(milliseconds);
+		timer.restart();
+	}
+}//end class StatusLabel
